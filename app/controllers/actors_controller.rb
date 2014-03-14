d6295adececeba4d9b17ea5356622ae9b88d0c10
Damon Jarett Jacoby <damonjacoby@gmail.com>
2014-03-13 21:09:28 -0700

    add bootstrap, add search bar, add images, add partial list

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,9 +1,25 @@
 class ActorsController < ApplicationController
   def index
-    @actors = Actor.all
-    @actors = Actor.search(params[:search])
-    @movies = Movie.all
+   @actors = Actor.all
+    # @actors = Actor.search(params[:search])
 
+    if params[:search]
+      
+      @movies = Movie.all
+
+      @actor = Actor.where("name LIKE ?", "%#{params[:search].capitalize}%")
+    else
+      @actor = Actor.all
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
+  end
+
+  def create
   end
 
   def new
