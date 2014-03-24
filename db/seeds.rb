48c17b79bc909e9494f96fd03b9ece92ddde4076
Damon Jarett Jacoby <damonjacoby@gmail.com>
2014-03-24 16:17:40 -0700

    seed herokuquotes and RT

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,54 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+a = Actor.find_by(name: "Matt Damon")
+a.quote = "Listen, here's the thing. If you can't spot the sucker in the first half hour at the table, then you ARE the sucker."
+a.save
 
-movies = []
-# Enter Actor ID to get grab all information
-data = HTTParty.get "https://api.themoviedb.org/3/person/1892?api_key=631d0d27d8bd582b2876ac8036e7641b"
-actor = Actor.create(name: data['name'], star_id: data['id'], birthdate: data['birthday'], picture: data['profile_path'], bio: data['biography'], dvd: data['$_domestic_DVD_sales'], budget: data['movie_financial_summary_production_budget'])
+b = Movie.find_by(title: "Good Will Hunting")
+b.freshness = 97
+b.save
 
-response = JSON.parse(File.read('db/matt.json'))
+c = Movie.find_by(title: "True Grit")
+c.freshness = 96
+c.save
 
-for i in 0...response.count
-  movie = Movie.find_or_initialize_by(title: (response[i]["movie_display_name"]))
-  if movie.new_record?
-    movie.title = response[i]["movie_display_name"]
-    movie.total_revenue = response[i]["$_total_box_office"]
-    movie.usa_revenue = response[i]["movie_financial_summary_domestic_box_office"]
-    movie.international_revenue = response[i]["movie_financial_summary_international_box_office"]
-    movie.budget = response[i]["movie_financial_summary_production_budget"]
-    movie.release_year = response[i]["movie_production_year"]
-    movies << movie
-    movie.save
-  end
-  # Associate this movie to the current actor/actresses
-  actor.movies << movie
-  actor.save
-end
+d = Movie.find_by(title: "The Bourne Ultimatum")
+d.freshness = 94
+d.save
+
+e = Movie.find_by(title: "All the Pretty Horses")
+e.freshness =32
+e.save
+
+f = Movie.find_by(title: "The Third Wheel")
+f.freshness = 33
+f.save
+
+g = Movie.find_by(title: "The Monuments Men")
+g.freshness = 33
+g.save
+
+# movies = []
+# # Enter Actor ID to get grab all information
+# data = HTTParty.get "https://api.themoviedb.org/3/person/1892?api_key=631d0d27d8bd582b2876ac8036e7641b"
+# actor = Actor.create(name: data['name'], star_id: data['id'], birthdate: data['birthday'], picture: data['profile_path'], bio: data['biography'], dvd: data['$_domestic_DVD_sales'], budget: data['movie_financial_summary_production_budget'])
+
+# response = JSON.parse(File.read('db/matt.json'))
+
+# for i in 0...response.count
+#   movie = Movie.find_or_initialize_by(title: (response[i]["movie_display_name"]))
+#   if movie.new_record?
+#     movie.title = response[i]["movie_display_name"]
+#     movie.total_revenue = response[i]["$_total_box_office"]
+#     movie.usa_revenue = response[i]["movie_financial_summary_domestic_box_office"]
+#     movie.international_revenue = response[i]["movie_financial_summary_international_box_office"]
+#     movie.budget = response[i]["movie_financial_summary_production_budget"]
+#     movie.release_year = response[i]["movie_production_year"]
+#     movies << movie
+#     movie.save
+#   end
+#   # Associate this movie to the current actor/actresses
+#   actor.movies << movie
+#   actor.save
+# end
