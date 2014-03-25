96b6437ef5cb88ba5780075ff941d2bee167bd23
Damon Jarett Jacoby <damonjacoby@gmail.com>
2014-03-25 11:35:23 -0700

    seed Heroku please

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
 # Movie.destroy_all
 movies = []
 # Enter Actor ID to get grab all information
-data = HTTParty.get "https://api.themoviedb.org/3/person/6193?api_key=631d0d27d8bd582b2876ac8036e7641b"
-actor = Actor.create(name: data['name'], star_id: data['id'], birthdate: data['birthday'], picture: data['profile_path'], bio: data['biography'], dvd: data['$_domestic_DVD_sales'], budget: data['movie_financial_summary_production_budget'], quote: "I'm the king of the world!" )
+data = HTTParty.get "https://api.themoviedb.org/3/person/1204?api_key=631d0d27d8bd582b2876ac8036e7641b"
+actor = Actor.create(name: data['name'], star_id: data['id'], birthdate: data['birthday'], picture: data['profile_path'], bio: data['biography'], dvd: data['$_domestic_DVD_sales'], budget: data['movie_financial_summary_production_budget'], quote: "After all... I'm just a girl, standing in front of a boy, asking him to love her." )
 
-response = JSON.parse(File.read('db/leo.json'))
+response = JSON.parse(File.read('db/julia.json'))
 
 for i in 0...response.count
   movie = Movie.find_or_initialize_by(title: (response[i]["movie_display_name"]))
