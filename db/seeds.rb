72509fa09e54e2a850a903e99dfd5ec78e7db4b1
Damon Jarett Jacoby <damonjacoby@gmail.com>
2014-03-25 11:13:50 -0700

    seed Heroku please

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
 # Movie.destroy_all
 movies = []
 # Enter Actor ID to get grab all information
-data = HTTParty.get "https://api.themoviedb.org/3/person/2157?api_key=631d0d27d8bd582b2876ac8036e7641b"
-actor = Actor.create(name: data['name'], star_id: data['id'], birthdate: data['birthday'], picture: data['profile_path'], bio: data['biography'], dvd: data['$_domestic_DVD_sales'], budget: data['movie_financial_summary_production_budget'], quote: "You treat a disease, you win, you lose. You treat a person, I'll guarantee you'll win." )
+data = HTTParty.get "https://api.themoviedb.org/3/person/3223?api_key=631d0d27d8bd582b2876ac8036e7641b"
+actor = Actor.create(name: data['name'], star_id: data['id'], birthdate: data['birthday'], picture: data['profile_path'], bio: data['biography'], dvd: data['$_domestic_DVD_sales'], budget: data['movie_financial_summary_production_budget'], quote: "Jarvis, sometimes you gotta run before you can walk" )
 
-response = JSON.parse(File.read('db/robin.json'))
+response = JSON.parse(File.read('db/robert.json'))
 
 for i in 0...response.count
   movie = Movie.find_or_initialize_by(title: (response[i]["movie_display_name"]))
