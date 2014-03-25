25074cb7c7130f359a199fc7a84bd36b9c5ba2a2
Damon Jarett Jacoby <damonjacoby@gmail.com>
2014-03-25 10:12:41 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
 # Movie.destroy_all
 movies = []
 # Enter Actor ID to get grab all information
-data = HTTParty.get "https://api.themoviedb.org/3/person/31?api_key=631d0d27d8bd582b2876ac8036e7641b"
-actor = Actor.create(name: data['name'], star_id: data['id'], birthdate: data['birthday'], picture: data['profile_path'], bio: data['biography'], dvd: data['$_domestic_DVD_sales'], budget: data['movie_financial_summary_production_budget'], quote: "My mama always said life was like a box of chocolates. You never know what you're gonna get." )
+data = HTTParty.get "https://api.themoviedb.org/3/person/2231?api_key=631d0d27d8bd582b2876ac8036e7641b"
+actor = Actor.create(name: data['name'], star_id: data['id'], birthdate: data['birthday'], picture: data['profile_path'], bio: data['biography'], dvd: data['$_domestic_DVD_sales'], budget: data['movie_financial_summary_production_budget'], quote: "The path of the righteous man is beset on all sides by the inequities of the selfish and the tyranny of evil men. Blessed is he who in the name of charity and goodwill, shepherds the weak through the valley of the darkness, for he is truly his brother’s keeper, and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy my brothers. And you will know I am the Lord when I lay My vengeance upon thee." )
 
-response = JSON.parse(File.read('db/tom_hanks.json'))
+response = JSON.parse(File.read('db/samuel.json'))
 
 for i in 0...response.count
   movie = Movie.find_or_initialize_by(title: (response[i]["movie_display_name"]))
@@ -33,26 +33,26 @@ for i in 0...response.count
 end
 
 
-a = Movie.find_by(title: "Toy Story")
-a.freshness = 100
+a = Movie.find_by(title: "The Player")
+a.freshness = 98
 a.save
 
-a = Movie.find_by(title: "Toy Story 2")
-a.freshness = 100
+a = Movie.find_by(title: "Pulp Fiction")
+a.freshness = 94
 a.save
 
-a = Movie.find_by(title: "Catch Me if You Can")
-a.freshness = 96
+a = Movie.find_by(title: "Moonrise Kingdom")
+a.freshness = 94
 a.save
 
-a = Movie.find_by(title: "Angels & Demons")
-a.freshness = 37
+a = Movie.find_by(title: "Perfect Stranger")
+a.freshness = 11
 a.save
 
-a = Movie.find_by(title: "Larry Crowne")
-a.freshness = 35 
+a = Movie.find_by(title: "The Cold Light of Day")
+a.freshness = 5
 a.save
 
-a = Movie.find_by(title: "The Da Vinci Code")
-a.freshness = 25
+a = Movie.find_by(title: "The Whole Ten Yards")
+a.freshness = 4
 a.save
