288be766160b680487c48c80e9fe4587513176df
Damon Jarett Jacoby <damonjacoby@gmail.com>
2014-03-25 12:02:47 -0700

    seed Heroku please

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
 # Movie.destroy_all
 movies = []
 # Enter Actor ID to get grab all information
-data = HTTParty.get "https://api.themoviedb.org/3/person/10990?api_key=631d0d27d8bd582b2876ac8036e7641b"
-actor = Actor.create(name: data['name'], star_id: data['id'], birthdate: data['birthday'], picture: data['profile_path'], bio: data['biography'], dvd: data['$_domestic_DVD_sales'], budget: data['movie_financial_summary_production_budget'], quote: "At least no one on the Gryffindor team had to buy their way in. They got in on pure talent." )
+data = HTTParty.get "https://api.themoviedb.org/3/person/776?api_key=631d0d27d8bd582b2876ac8036e7641b"
+actor = Actor.create(name: data['name'], star_id: data['id'], birthdate: data['birthday'], picture: data['profile_path'], bio: data['biography'], dvd: data['$_domestic_DVD_sales'], budget: data['movie_financial_summary_production_budget'], quote: "That's right, fool! Now I'm a FLYING talking donkey! You might have seen a housefly, maybe even a superfly, but I bet you ain't never seen a DONKEY fly! Ha, ha!" )
 
-response = JSON.parse(File.read('db/emma.json'))
+response = JSON.parse(File.read('db/eddie_murphy.json'))
 
 for i in 0...response.count
   movie = Movie.find_or_initialize_by(title: (response[i]["movie_display_name"]))
