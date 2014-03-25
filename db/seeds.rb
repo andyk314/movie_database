f93cebd4091004a35da1d741c8bb5710d1f2efcf
Damon Jarett Jacoby <damonjacoby@gmail.com>
2014-03-25 09:25:48 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
 # Movie.destroy_all
 movies = []
 # Enter Actor ID to get grab all information
-data = HTTParty.get "https://api.themoviedb.org/3/person/72179?api_key=631d0d27d8bd582b2876ac8036e7641b"
-actor = Actor.create(name: data['name'], star_id: data['id'], birthdate: data['birthday'], picture: data['profile_path'], bio: data['biography'], dvd: data['$_domestic_DVD_sales'], budget: data['movie_financial_summary_production_budget'], quote: "Listen, here's the thing. If you can't spot the sucker in the first half hour at the table, then you ARE the sucker." )
+data = HTTParty.get "https://api.themoviedb.org/3/person/500?api_key=631d0d27d8bd582b2876ac8036e7641b"
+actor = Actor.create(name: data['name'], star_id: data['id'], birthdate: data['birthday'], picture: data['profile_path'], bio: data['biography'], dvd: data['$_domestic_DVD_sales'], budget: data['movie_financial_summary_production_budget'], quote: "In this life, it's not what you hope for, it's not what you deserve- it's what you take!" )
 
-response = JSON.parse(File.read('db/jennifer.json'))
+response = JSON.parse(File.read('db/tom.json'))
 
 for i in 0...response.count
   movie = Movie.find_or_initialize_by(title: (response[i]["movie_display_name"]))
@@ -33,32 +33,32 @@ for i in 0...response.count
 end
 
 
-b = Movie.find_by(title: "Winter's Bone")
-b.freshness = 94
+b = Movie.find_by(title: "Risky Business")
+b.freshness = 98
 b.save
 puts "b"
 
-c = Movie.find_by(title: "American Hustle")
+c = Movie.find_by(title: "Mission: Impossible - Ghost Protocol")
 c.freshness = 93
 c.save
 puts "c"
 
-d = Movie.find_by(title: "Silver Linings Playbook")
+d = Movie.find_by(title: "The Color of Money")
 d.freshness = 92
 d.save
 puts "d"
 
-e = Movie.find_by(title: "The Beaver")
-e.freshness = 60
+e = Movie.find_by(title: "Endless Love")
+e.freshness = 25
 e.save
 puts "e"
 
-g = Movie.find_by(title: "The Burning Plain")
-g.freshness = 35
+g = Movie.find_by(title: "Losin' It")
+g.freshness = 22
 g.save
 puts "g"
 
-f = Movie.find_by(title: "House at the End of the Street")
-f.freshness = 10
+f = Movie.find_by(title: "Cocktail")
+f.freshness = 5
 f.save
 puts "f"
