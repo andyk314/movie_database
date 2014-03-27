9e8f55ca20e25eac1ff6dd5a4947611a44b83ff3
Damon Jarett Jacoby <damonjacoby@gmail.com>
2014-03-27 09:07:35 -0700

    fix JQ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Moviedb::Application.routes.draw do
+  get "welcome/index"
   resources :actors
+  resources :welcome
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
